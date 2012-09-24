6f9946964daa826eb5725b35f2c705d8e3a1ecbf
Maurits van Rees <maurits@vanrees.org>
2012-09-24 18:23:08 +0200

    pep8

--- setup.py
+++ setup.py
@@ -5,17 +5,17 @@ version = '1.3.dev0'
 setup(name='collective.noindexing',
       version=version,
       description="Ignore all indexing operations",
-      long_description=open("README.txt").read() + "\n" +
-                       open("CHANGES.rst").read(),
+      long_description=(open("README.txt").read() + "\n" +
+                        open("CHANGES.rst").read()),
       # Get more strings from
       # http://pypi.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
-        "Framework :: Plone",
-        "Framework :: Plone :: 3.3",
-        "Framework :: Plone :: 4.0",
-        "Framework :: Plone :: 4.1",
-        "Programming Language :: Python",
-        ],
+          "Framework :: Plone",
+          "Framework :: Plone :: 3.3",
+          "Framework :: Plone :: 4.0",
+          "Framework :: Plone :: 4.1",
+          "Programming Language :: Python",
+          ],
       keywords='',
       author='Maurits van Rees',
       author_email='m.van.rees@zestsoftware.nl',
